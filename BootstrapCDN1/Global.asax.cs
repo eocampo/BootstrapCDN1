966304537f5b8073213b56e16e457c36ce4a18c2
Ernesto Ocampo <ernesto@sistrategia.com>
2018-02-10 03:20:13 -0600

    Add BootstrapCDN1 project initial files

---
+++ BootstrapCDN1/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace BootstrapCDN1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e) {
+            AreaRegistration.RegisterAllAreas();
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ BootstrapCDN1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(BootstrapCDN1.Startup))]
+
+namespace BootstrapCDN1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app) {
+            
+        }
+    }
+}
